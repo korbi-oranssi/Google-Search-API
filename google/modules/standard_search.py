25c63d73eb3d8bcc060ee5dbc6e42ad036ecc707
Paktalin <likorn@ttu.ee>
2018-11-01 00:02:46 +0100

    test fix 1

--- google/modules/standard_search.py
+++ google/modules/standard_search.py
@@ -66,6 +66,7 @@ def search(query, pages=1, lang='en', area='com', ncr=False, void=True, time_per
     results = []
     for i in range(pages):
         url = _get_search_url(query, i, lang=lang, area=area, ncr=ncr, time_period=time_period)
+        print("search url is: ", url)
         html = get_html(url)
 
         if html:
