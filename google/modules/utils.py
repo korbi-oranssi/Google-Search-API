10e31e23f6e2959e526b7a9b3d83912d3b76e9d9
Paktalin <likorn@ttu.ee>
2018-11-01 00:08:13 +0100

    test fix 2

--- google/modules/utils.py
+++ google/modules/utils.py
@@ -419,8 +419,9 @@ def _get_search_url(query, page=0, per_page=10, lang='en', area='com', ncr=False
         else:
             raise AreaError('invalid  name,  no area found')
         url += params
-    #return url
-    return "https://www.google.it/search?hl=en&as_q=eesti&as_epq=&as_oq=&as_eq=&as_nlo=&as_nhi=&lr=lang_et&cr=&as_qdr=all&as_sitesearch=&as_occt=any&safe=images&as_filetype=&as_rights="
+    params = "hl=en&as_q=eesti&as_epq=&as_oq=&as_eq=&as_nlo=&as_nhi=&lr=lang_et&cr=&as_qdr=all&as_sitesearch=&as_occt=any&safe=images&as_filetype=&as_rights="
+    url += params
+    return url
 
 
 def get_html(url):
